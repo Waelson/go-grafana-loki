8e2bbf9670eb75fdb47a9f2f1f6cf2a6575b5b6b
waelson <waelson@gmail.com>
2024-11-05 23:30:27 -0300

    add o11y

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,15 +1,24 @@
 package main
 
 import (
+	"fmt"
 	"github.com/Waelson/go-grafana-loki/internal/handlers"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/go-chi/chi/v5"
 )
 
 func main() {
 	controller := handlers.NewController()
+
+	go func() {
+		for {
+			fmt.Println(`{"message":"log message","level":"info","thread":"main"}`)
+			time.Sleep(1 * time.Second)
+		}
+	}()
 
 	r := chi.NewRouter()
 	// r.Use(middleware.Logger)
--- internal/handlers/controllers.go
+++ internal/handlers/controllers.go
@@ -1,10 +1,27 @@
 package handlers
 
 import (
+	"encoding/json"
 	"github.com/go-chi/chi/v5"
 	"log"
 	"net/http"
 )
+
+type LogMessage struct {
+	App     string `json:"app"`
+	Level   string `json:"level"`
+	Message string `json:"message"`
+}
+
+func logJSON(app, level, message string) {
+	logMsg := LogMessage{App: app, Level: level, Message: message}
+	jsonData, err := json.Marshal(logMsg)
+	if err != nil {
+		log.Printf("Error marshalling JSON log: %v", err)
+		return
+	}
+	log.Println(string(jsonData))
+}
 
 type Controller struct{}
 
@@ -14,13 +31,13 @@ func NewController() *Controller {
 
 func (c *Controller) Home(w http.ResponseWriter, r *http.Request) {
 	msg := "Home route accessed"
-	log.Printf("app=%s level=%s message=\"%s\"", "golang-app", "info", msg)
+	logJSON("golang-app", "info", msg)
 	w.Write([]byte(msg))
 }
 
 func (c *Controller) Health(w http.ResponseWriter, r *http.Request) {
 	msg := "Health route accessed"
-	log.Printf("app=%s level=%s message=\"%s\"", "golang-app", "info", msg)
+	logJSON("golang-app", "info", msg)
 	w.Write([]byte(msg))
 }
 
